1e8e2f7d7a5b31fc27050896791b80b0e7f9aed0
Nazia R <nazia.huma@gmail.com>
2021-07-15 03:48:34 +0400

    moved public file inside src

--- src/controllers/api/index.js
+++ src/controllers/api/index.js
@@ -1,38 +1,55 @@
-const { Workout } = require("../../models")
+const { Workout } = require("../../models");
 
-const getWorkouts = (req, res) => {
-  
+const getWorkouts = async (req, res) => {
+  try {
+    const lastWorkout = await Workout.aggregate([
+      { $addFields: { totalDuration: { $sum: "$exercises.duration" } } },
+    ]);
+    return res.json(lastWorkout);
+  } catch (error) {
+    console.log(error.message);
+    return res.status(500).json({ error: " Failed to get workouts " });
+  }
 };
 
 const updateWorkouts = (req, res) => {
-  try{
+  try {
     const { id } = req.prams;
     const exercise = req.body;
 
     const updateWorkout = Workout.findByIdAndUpdate(
-      id, 
-      { $push: {exercises: exercise}},
-      {new: true}
+      id,
+      { $push: { exercises: exercise } },
+      { new: true }
     );
-      return res.json(updateWorkout);
-  } catch(error){
-    console.log(error.message); 
+    return res.json(updateWorkout);
+  } catch (error) {
+    console.log(error.message);
   }
 };
 
-const createWorkouts = async(req, res) => {
+const createWorkouts = async (req, res) => {
   try {
-    const workout = await Workout.create()
-    return res.json(workout)
-
-  } catch(error) {
-    console.log(error.message)
-    return res.status(500).json({ error: "failed to get workouts"})
+    const workout = await Workout.create();
+    return res.json(workout);
+  } catch (error) {
+    console.log(error.message);
+    return res.status(500).json({ error: "failed to get workouts" });
   }
 };
 
-const getAggregatedWorkouts = (req, res) => {
-  
+const getAggregatedWorkouts = async (req, res) => {
+  try {
+    const lastWorkout = await Workout.aggregate([
+      { $addFields: { totalDuration: { $sum: "$exercises.duration" } } },
+    ])
+      .sort({ _id: -1 })
+      .limit(7);
+    return res.json(lastWorkout);
+  } catch (error) {
+    console.log(error.message);
+    return res.status(500).json({ error: " Failed to get workouts " });
+  }
 };
 
 module.exports = {
--- src/controllers/html/index.js
+++ src/controllers/html/index.js
@@ -1,15 +1,15 @@
 const path = require("path");
 
 const renderStats = (req, res) => {
-  res.send(path.join(__dirname, "../../../public/stats.html"));
+  res.send(path.join(__dirname, "../../public/stats.html"));
 };
 
 const renderExercise = (req, res) => {
-  res.send(path.join(__dirname, "../../../public/exercise.html"));
+  res.send(path.join(__dirname, "../../public/exercise.html"));
 };
 
 const renderHome = (req, res) => {
-  res.send(path.join(__dirname, "../../../public/index.html"));
+  res.send(path.join(__dirname, "../../public/index.html"));
 };
 
 module.exports = {
